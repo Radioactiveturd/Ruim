e7c29a2f6291476c00c993412b9dd192a14e6e48
Rasch <poopooc669@gmail.com>
2025-01-24 10:24:38 -0500

    Create 5RM.py

---
+++ 5RM.py
@@ -0,0 +1,192 @@
+# authors: Rash Marie Leduc <cleduc2818533@woonsocketschools.com> Zachary curran zcurran2801931@woonsocketschools.com
+#  date written: 11/21/2024
+import random
+print("epik character adventure! :D")
+print("by Zachary Curran and Rasch Leduc!")
+name = input("What is your character's name?: ")
+sided = 10
+fart = random.randint(1, 10)
+perception = random.randint(1, 10)
+endurance = random.randint(1, 10)
+charisma = random.randint(1, 10)
+intelligence = random.randint(1, 10)
+agility = random.randint(1, 10)
+luck = random.randint(1, 10)
+
+
+print(name, "your health is", sided)
+print(name, "your strength is", fart)
+print(name, "your perception is", perception)
+print(name, "your endurance is", endurance)
+print(name, "your charisma is", charisma)
+print(name, "your intelligence is",intelligence)
+print(name, "your agility is",agility)
+print(name, "your luck is",luck)
+print("You are in the town of Narium.. on a mission to defeat a feral beast known as the mutant corium monster"),
+print("that has been terrorizing this town, you head inside reactor Narium.")
+print("where it all happened... you see a stair way on your left, it is flooded with water..")
+print("on your right hand side is a some crates  and in front of you is a valve,")
+do = input("what will you do? put the name of item, crate or valve: ")
+if do == "valve":
+    print("the stair way on the left side of you drained,it is now unblocked")
+if do == "crate":
+    print("you found a water bottle, looks really strong..")
+    sorewilly = input("will you turn the valve as well?: ")
+    if sorewilly == "yes":
+        print("the stair way on the left side of you drained,it is now unblocked")
+print("you walk down the stair way and open the door you see a man in a hazmat covered in burns and 'ketchup' hes holding an axe and slowly coming closer.")
+guy = 10
+print("attacks you can use:")
+print("(1) slap")
+print("(2) punch")
+print("(3) hammer")
+attack = input("What attack will you use?: ")
+if attack == "3":
+    print("you did 6 damage!")
+if attack == "2":
+    print("you did 3 damage!")
+if attack == "1":
+    print("you did 3 damage!")
+print("the enemy attacked you and did 4 damage.")
+meow = input("What attack will you use?: ")
+if meow == "1":
+    print("the enemy fell down and is too hurt to fight")
+if meow == "3":
+    print("the enemy has fallen..")
+if meow =="2":
+    print("enemy did 7 damage!")
+    print("you have fallen..")
+print("the enemy dropped something on the ground!")
+uuu = input("will you pick it up?: ")
+if uuu  == "yes":
+    print("you picked up a stimpack.")
+    tickle = input("will you use your stimpack?: ")
+    if tickle == "yes":
+        print("you are back to full health!")
+    if tickle == "no":
+        burpingcow = sided - 4
+        print("you are at", burpingcow, "health")
+    if uuu == "no":
+        print("no health added")
+print("you see a door on the left and enter.")
+print("before you enter you see a rope above the door")
+poop = input("will you step over it?: ")
+if poop == "yes":
+    print("you stepped over it and avoided a trap!")
+if poop == "no":
+    print("and anvil fell on.. you have fallen..")
+print("in the room is another mutant")
+print("it stands in your path.. groaning in agony.")
+print("attacks you can use")
+print("(1) hammer")
+print("(2) punch")
+print("(3) Axe")
+doodoogram = input("what attack will you use?: ")
+if doodoogram == "1":
+    print("you did 6 damage!")
+if doodoogram == "2":
+    print("you did 3 damage")
+if doodoogram =="3":
+    print("you did 7 damage!")
+print("enemy attacked and did 5 damage!")
+doodoogram = input("what attack will you use?: ")
+if doodoogram == "1":
+    print("you did 6 damage, enemy has fallen..")
+if doodoogram == "2":
+        print("enemy did 5 damage!")
+        print("you have fallen..")
+if doodoogram =="3":
+    print("you did 7 damage! enemy has fallen..")
+print("enemy dropped a cat!")
+print("you picked up the cat. (YOU HAVE NO CHOICE.)")
+print("you enter the next door and see Unpleasant they look like a thin rectangle with a gradient consisting of the colors green, pink, and sort of orange-brown. ")
+print("they seem interested in the cat you are holding.")
+stinky = input("will you give unpleasant your cat?: ")
+if stinky == "yes":
+    print("you give them the cat and they eat it in your face. ")
+if stinky == "no" :
+    print("they take it anyway and eat it.")
+print("unpleasant has become your companion!")
+print("you have a bad feeling about the next room")
+print("you enter the next room and in the middle of the room is a large jumbled conjunction of corium, sticking out are heads, arms and legs wriggling out at you and your companion.")
+print("a mutated arm reaches its hand out to you smashing you into the concrete wall.")
+print("unpleasant slowly waddles its way towards you injecting a stimpack into your arm healing you at full health")
+print("the cold air blows on your body filling you with determination.")
+print("you get back up and prepare for war!")
+print("ENEMY HEALTH 20")
+walter = 20
+print("attacks you can use")
+print("(1) spud tanto = 6 damage")
+print("(2) uranium slash = 7 damage")
+print("(3) unpleasant gluttony = 6")
+print("(4) pest sting = 5")
+bam = input("what attack will you use?: ")
+if bam == "1":
+    zachary = walter - 6
+    print("you used spud tanto and did 6 damage!")
+if bam == "2":
+    zachary = walter - 7
+    print("you used uranium slash and did 7 damage!")
+if bam == "3":
+    zachary = walter - 6
+    print("you used unpleasant gluttony and did 6 damage!")
+if bam == "4":
+    zachary = walter - 5
+    print("you used pest sting and did 5 damage!")
+    print("enemy did ERROR damage!")
+    print("you have stopped fallen..")
+print("enemy used corium smash and did 6 damage!")
+beavis = input("what attack will you use?: ")
+if beavis == "1":
+    zachary = walter - 6
+    print("you used spud tanto and did 6 damage!")
+if beavis == "2":
+    zachary = walter - 7
+    print("you used uranium slash and did 7 damage!")
+if beavis == "3":
+    zachary = walter - 6
+    print("you used unpleasant gluttony and did 6 damage!")
+if beavis == "4":
+    zachary = walter - 5
+    print("you used pest sting and did 5 damage!")
+    print("enemy did 6 damage!")
+print("unpleasant gave you a stimpack!")
+print("full health!")
+ddd = input("what attack will you use?: ")
+if ddd == "1":
+    zachary = walter - 6
+    print("you used spud tanto and did 6 damage!")
+if ddd == "2":
+    zachary = walter - 7
+    print("you used uranium slash and did 7 damage!")
+if ddd == "3":
+    zachary = walter - 6
+    print("you used unpleasant gluttony and did 6 damage!")
+if ddd == "4":
+    zachary = walter - 5
+    print("you used pest sting and did 5 damage!")
+print("the enemy is weak, quick this is your chance!")
+print("(1) Mercy")
+print("(2) Unpleasant")
+if do =="crate":
+    print("(3) Water bottle")
+ending = input("what will you use?: ")
+if ending == "1":
+    print("you decide to spare the corium monster..")
+    print("... it slowly starts melt until it became nothing.. down the drain to reveal a a glowing white orb..")
+    print("i'm sorry.. for everything.. hurting this town.. the people.. my following.. you..")
+    print("you should have never spared me. it's not deserved..")
+    print("thank you.. for everything..")
+    print("Congratulations you got the happy ending! :D")
+    print("THE END!")
+if ending =="2":
+    print("unpleasant slowly walks uo to the weakened being..")
+    print("unpleasant eats the corium monster (because he's a menace)")
+    print("BOOWOMP! you got a the unpleasant ending, i guess this is fine?")
+    print("THE END")
+if ending =="3":
+    print("you walk up to the weakened monster as it stares at you with horror..")
+    print("you open the bottle of water and pour it on the monster..")
+    print("it starts steaming then... KABOOM! it blew up.. exploding half of the world..")
+    print("uh oh.. ermm.. you got the bad ending.. sorry lad, water and corium dont mix..")
+    print("THE END")
